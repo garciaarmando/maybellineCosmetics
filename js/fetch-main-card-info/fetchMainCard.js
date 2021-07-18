5a773c700672b34ea0546aa070ac6f7036768e27
Armando García <armandobfmv@gmail.com>
2021-07-17 20:56:42 -0500

    refactoring the painting of the data coming from the api

--- js/fetch-main-card-info/fetchMainCard.js
+++ js/fetch-main-card-info/fetchMainCard.js
@@ -1,8 +1,9 @@
 let d = document;
-
+//createElement en lugar de usar el template
+//colocarle a img un width y un height
 function fetchMainCardInfo() {
     const $mainItemContainer = d.querySelector(".main-item-container"),
-        $template = d.getElementById("template-card").content,
+        // $template = d.getElementById("template-card").content,
         $fragment = d.createDocumentFragment();
     async function getData() {
         try {
@@ -11,15 +12,31 @@ function fetchMainCardInfo() {
                 ),
                 json = await res.json();
             json.forEach((el) => {
-                $template.querySelector("img").setAttribute("src", el.image_link);
-                $template.querySelector("img").setAttribute("alt", "Product Image");
-                $template.querySelector("h3").textContent = `${el.name} | ${el.price}`;
-                $template.querySelector(
-                    "h2"
-                ).textContent = `${el.brand} | ${el.category} | ${el.product_type}`;
-                $template.querySelector("p").textContent = el.description;
-                let $clone = d.importNode($template, true);
-                $fragment.appendChild($clone);
+                const $section = d.createElement("section"),
+                    $figure = d.createElement("figure"),
+                    $img = d.createElement("img"),
+                    $h3 = d.createElement("h3"),
+                    $hr = d.createElement("hr"),
+                    $h2 = d.createElement("h2"),
+                    $p = d.createElement("p");
+                $section.classList.add("main-item-card");
+                $figure.classList.add("main-img-container");
+                $h3.classList.add("main-item-name");
+                $h2.classList.add("main-item-specs");
+                $p.classList.add("main-item-desc");
+                $img.setAttribute("src", `${el.image_link}`);
+                $img.setAttribute("alt", "Product image");
+                $h3.textContent = `${el.name} | ${el.price}`;
+                $h2.textContent = `${el.brand} | ${el.category} | ${el.product_type}`;
+                $p.textContent = el.description;
+                $p.style.setProperty("margin-block-end", "0.1rem");
+                $figure.appendChild($img);
+                $section.appendChild($figure);
+                $section.appendChild($h3);
+                $section.appendChild($hr);
+                $section.appendChild($h2);
+                $section.appendChild($p);
+                $fragment.appendChild($section);
             });
             $mainItemContainer.appendChild($fragment);
         } catch (err) {
