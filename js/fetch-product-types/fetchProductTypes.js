3d5ff04360366bf365273aaa8a4b48ded0b4cd9d
Armando García <armandobfmv@gmail.com>
2021-07-21 13:37:36 -0500

    Fetching the data to the product types page

--- js/fetch-product-types/fetchProductTypes.js
+++ js/fetch-product-types/fetchProductTypes.js
@@ -1,1 +1,44 @@
 const d = document;
+
+function fetchProdyctTypes() {
+    const $mainProductTypesContainer = d.getElementById(
+            "main-productTypes-container"
+        ),
+        $fragment = d.createDocumentFragment();
+    async function getData() {
+        try {
+            const res = await fetch(
+                    "https://makeup-api.herokuapp.com/api/v1/products.json"
+                ),
+                json = await res.json(),
+                setObj = new Set(),
+                uniques = json.reduce((acc, el) => {
+                    if (!setObj.has(el.product_type)) {
+                        setObj.add(el.product_type, el);
+                        acc.push(el);
+                    }
+                    return acc;
+                }, []);
+            uniques.forEach((el) => {
+                const $section = d.createElement("section"),
+                    $anchor = d.createElement("a");
+                $section.classList.add("main-productTypes-item");
+                $anchor.setAttribute("href", `${el.product_link}`);
+                $anchor.setAttribute("target", "_blank");
+                $anchor.setAttribute("rel", "noopener");
+                $anchor.textContent = `${el.product_type}`;
+                $anchor.textContent = $anchor.textContent.replace(/_/g, " ");
+                $anchor.textContent = $anchor.textContent.toUpperCase();
+                $section.appendChild($anchor);
+                $fragment.appendChild($section);
+            });
+            $mainProductTypesContainer.appendChild($fragment);
+        } catch (err) {
+            let errorMessage =
+                err.statusText || "Ocurrió un error al tratar de obtener los datos";
+            $mainProductTypesContainer.innerHTML = `<p>Error: ${errorMessage}</p>`;
+        } finally {}
+    }
+    getData();
+}
+fetchProdyctTypes();
