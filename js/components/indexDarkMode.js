011c97ad3999217c885d90b8369a72ddd8bf335a
Armando García <armandobfmv@gmail.com>
2021-07-24 20:48:33 -0500

    Dark mode to all screen sizes

--- js/components/indexDarkMode.js
+++ js/components/indexDarkMode.js
@@ -44,7 +44,7 @@ export default function darkMode(
         });
         ls.setItem("theme", "dark");
     };
-    if (hour < 5 || hour >= 20) {
+    if (hour < 5 || hour >= 18) {
         enableDarkMode();
     } else {
         enableLightMode();
